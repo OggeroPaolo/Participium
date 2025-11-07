20a29532cbbe2b3134313caa4589a86167de5d48
Federico Sciascia <s333188@studenti.polito.it>
2025-11-07 22:13:34 +0100

    [ADD] user-registrations API

--- Back-end/src/routes/index.ts
+++ Back-end/src/routes/index.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
 import healthRoutes from "./health.routes.js";
+import registrationsRoutes from "./registrations.routes.js";
 
 const router = Router();
 
 // Mount route modules
 router.use(healthRoutes);
-
-// Additional route modules will be added here as the project grows
-// Example:
-// import userRoutes from "./user.routes.js";
-// router.use("/users", userRoutes);
+router.use(registrationsRoutes);
 
 export default router;
 
