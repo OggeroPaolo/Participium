3675761887095412555917c37d47a4d2e39884ee
Federico Sciascia <s333188@studenti.polito.it>
2025-11-08 18:33:09 +0100

    [ADD] handleSignup API implementation

--- Front-end/src/API/API.js
+++ Front-end/src/API/API.js
@@ -2,12 +2,29 @@ const URI = "http://localhost:3000";
 
 // Register a new user
 async function handleSignup(credentials) {
-  // TODO
+  const { firstName, lastName, username, email, firebaseToken, firebaseUid } = credentials;
+
+  const response = await fetch(`${URI}/user-registrations`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${firebaseToken}`
+    },
+    body: JSON.stringify({
+      firebaseUid: firebaseUid,
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      email: email
+    })
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.error || "Failed to register user");
+  }
+
+  return await response.json();
 }
 
-// Login a user
-async function handleLogin(credentials) {
-  // TODO
-}
-
-export { handleSignup, handleLogin };
+export { handleSignup };
