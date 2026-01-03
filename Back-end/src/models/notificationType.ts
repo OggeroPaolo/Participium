9e58580fece2fd597af54506e067cd52691d25dc
Federico Sciascia <s333188@studenti.polito.it>
2026-01-03 12:01:16 +0100

    [ADD] NotificationType

---
+++ Back-end/src/models/notificationType.ts
@@ -0,0 +1,8 @@
+export enum NotificationType {
+    CommentOnCreatedReport = "comment_on_created_report",
+    CommentOnAssignedReport = "comment_on_assigned_report",
+    StatusUpdate = "status_update",
+    ReportAssigned = "report_assigned",
+    ReportReviewed = "report_reviewed",
+    ReportRejected = "report_rejected",
+}
